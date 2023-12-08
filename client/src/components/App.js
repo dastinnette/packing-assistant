254016a9b658c50fe39468fb90f395715dcc4153
David Stinnette <dastinnette@gmail.com>
2023-12-07 22:33:14 -0500

    Imported bootstrap, created signup component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+// import { Switch, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Signup from "./Signup"
 
 function App() {
-  return <h1>Project Client</h1>;
+
+
+  return <Signup />
 }
 
 export default App;
+ 
